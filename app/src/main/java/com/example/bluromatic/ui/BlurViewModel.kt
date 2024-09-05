588fb5c4a518ed1f7c2b3b0a79c6b398c381b799
Klim Sukhikh <40332350+sirmong@users.noreply.github.com>
2024-09-05 21:54:53 +0200

    feat: show blurred image

--- app/src/main/java/com/example/bluromatic/ui/BlurViewModel.kt
+++ app/src/main/java/com/example/bluromatic/ui/BlurViewModel.kt
@@ -16,6 +16,7 @@
 
 package com.example.bluromatic.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
@@ -24,6 +25,7 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.work.WorkInfo
 import com.example.bluromatic.BluromaticApplication
+import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.data.BlurAmountData
 import com.example.bluromatic.data.BluromaticRepository
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -43,9 +45,12 @@ class BlurViewModel(private val bluromaticRepository: BluromaticRepository) : Vi
     //    val blurUiState: StateFlow<BlurUiState> = MutableStateFlow(BlurUiState.Default)
     val blurUiState: StateFlow<BlurUiState> = bluromaticRepository.outputWorkInfo
         .map { info ->
+            val outputImageUri = info.outputData.getString(KEY_IMAGE_URI)
+            Log.d("debug", outputImageUri ?: "")
             when {
-                info.state.isFinished -> {
-                    BlurUiState.Complete(outputUri = "")
+                info.state.isFinished && !outputImageUri.isNullOrEmpty() -> {
+                    Log.d("debug", "we are finished. output is $outputImageUri")
+                    BlurUiState.Complete(outputUri = outputImageUri)
                 }
 
                 info.state == WorkInfo.State.CANCELLED -> {
--- app/src/main/java/com/example/bluromatic/ui/BluromaticScreen.kt
+++ app/src/main/java/com/example/bluromatic/ui/BluromaticScreen.kt
@@ -19,10 +19,12 @@ package com.example.bluromatic.ui
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
@@ -34,6 +36,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
@@ -125,7 +128,9 @@ fun BluromaticScreenContent(
         BlurActions(
             blurUiState = blurUiState,
             onStartClick = { applyBlur(selectedValue) },
-            onSeeFileClick = {},
+            onSeeFileClick = { currentUri ->
+                showBlurredImage(context, currentUri)
+            },
             onCancelClick = { cancelWork() },
             modifier = Modifier.fillMaxWidth()
         )
@@ -159,6 +164,7 @@ private fun BlurActions(
                     Text(stringResource(R.string.start))
                 }
             }
+
             is BlurUiState.Loading -> {
                 FilledTonalButton(onCancelClick) {
                     Text(stringResource(R.string.cancel_work))
@@ -167,12 +173,18 @@ private fun BlurActions(
                     )
                 }
             }
+
             is BlurUiState.Complete -> {
+                Log.d("debug", "rendering additional button")
                 Button(
                     onClick = onStartClick,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier.width(200.dp)
                 ) {
                     Text(stringResource(R.string.start))
+                }
+                Spacer(modifier = Modifier.width(dimensionResource(R.dimen.padding_small)))
+                FilledTonalButton({ onSeeFileClick(blurUiState.outputUri) }) {
+                    Text(stringResource(R.string.see_file))
                 }
             }
         }
